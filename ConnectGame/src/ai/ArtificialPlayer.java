3fd90ed99fc0dd80eb37b0bcfb4264f44439c93f
MrM3s33k5 <47454844+MrM3s33k5@users.noreply.github.com>
2019-03-01 02:33:10 -0600

    Commented abSearch Function

--- ConnectGame/src/ai/ArtificialPlayer.java
+++ ConnectGame/src/ai/ArtificialPlayer.java
@@ -65,36 +65,72 @@ public class ArtificialPlayer implements Player {
     public boolean isTurn() {
         return turn;
     }
+    
+    /**
+     * The top level max function for the Artificial Player class' Game
+     * Tree.  abSearch calls the abMinValue function that starts the recursive
+     * traversal of the Game Tree.    
+     *  
+     * @return integer value indicating the AI's best move
+     * @param StateNode root node object
+     */
 
     private int abSearch(StateNode root)
     {
-        
+        // Random Integer Generator used to select column when AI cannot win
         Random r = new Random();
         
         //holder for all columns that can be placed into
-        //used in a final catch all in the case the a never changes from -1
+        //used in a final catch all in the case the action variable 'a' never changes from -1
         ArrayList<Integer> goodColumns = new ArrayList<Integer>();
-
+        
+        // Declare and initialize the action variable to its initial state
         int a = -1;
+        
+        // Set Alpha and Beta values to -inf and +inf to be passed to children
         root.setAlpha(Integer.MIN_VALUE);
         root.setBeta(Integer.MAX_VALUE);
-
+        
+        // Iterate through all columns on the board and place AI's piece
         for(int i = 0; i<root.getBoard().getSpaces().length;i++)
         {
             boolean goodColumn = true;
+            
+            // Copy of parent board to be passed to children 
             Board b = root.getBoard().copy();
+
+            // Try/Catch block to check that column is not full, and place piece at 
+            // column i if it is not.
             try 
             {
+                // placePiece function from board class throws ColumnFullException
+                // If column is not full, copy board to be passed to child, else
+                // board is null.
                 b = (b.copy().placePiece(myPiece, i)?b.copy():null);
+
+                // Place next piece on board at location i if column empty
                 if(b!= null)
                     b.placePiece(myPiece, i);
-            } catch (ColumnFullException cfe){
+            } 
+            catch (ColumnFullException cfe){
                 goodColumn = false;
             }
+
+            // If column is empty and board is not null, pass new board to child in 
+            // abMinValue function.  
             if(goodColumn  && b != null)
             {
+                // Add good column to ArrayList for final catch
+                //FIXME:  We are adding a lot of columns here, and we never remove any.  
+                //        Maybe we should add bad columns in catch block?
                 goodColumns.add(i);
+
+                // Call recursive abMinValue function.  Store current child alpha value.
                 int newAlpha = abMinValue(new StateNode(b, root.getDepthInTree()+1), root.getAlpha(), root.getBeta());
+                
+                // If current child alpha value is greater than root alpha value, store current
+                // child alpha value in root alpha variable to be passed to next child. Also, 
+                // select current for loop iteration i value as current best action.
                 if(newAlpha > root.getAlpha())
                 { 
                     root.setAlpha(newAlpha);
@@ -102,8 +138,13 @@ public class ArtificialPlayer implements Player {
                 }
             }
         }
+
+        // If algorithm's best action is initial state, then the AI loses.  In this
+        // event, have AI select random columns so that player may finish out the game.
         if(a==-1) return goodColumns.get(r.nextInt(goodColumns.size()));
 
+        // After tree traversal complete, return best possible action as determined by 
+        // Min-Max functions.
         return a;
     }
 
