1475868c3e33c6eebcd9991c86bf0212ac3baf07
MrM3s33k5 <47454844+MrM3s33k5@users.noreply.github.com>
2019-03-02 20:49:35 -0600

    Makfile somewhat generating javadoc

--- makefile.bash
+++ makefile.bash
@@ -12,13 +12,13 @@ TESTER_PATH="./ConnectGame/src/test"
 if test -d ./bin
 then
    rm -r ./bin
-   echo "Old bin files removed"
+   echo -e "\nOld bin files removed"
 fi
 
 if test -d ./docs
 then 
     rm -r ./docs
-    echo "Old docs files removed"
+    echo -e "\nOld docs files removed"
 fi
 
 # Check whether bin and docs directories exist. 
@@ -27,18 +27,21 @@ fi
 if !(test -d ./bin)
 then
     mkdir ./bin
-    echo "Making new bin folder"
+    echo -e "\nMaking new bin folder"
 fi
 
 if !(test -d ./docs)
 then
     mkdir ./docs
-    echo "Making new docs folder"
+    echo -e "\nMaking new docs folder"
 fi
 
 # Compile project's java code.
 # commented because not working yet.
+echo -e "\nCompiling Project...\n"
 javac -d ./bin -sourcepath ./src $GAME_ELEMS_PATH/Game.java $GAME_ELEMS_PATH/Player.java $GAME_ELEMS_PATH/Board.java $GAME_ELEMS_PATH/QuadraticBoard.java $GAME_ELEMS_PATH/InputPlayer.java $GAME_ELEMS_PATH/Piece.java $GAME_ELEMS_PATH/StandardBoard.java $AI_PATH/ArtificialPlayer.java $AI_PATH/helper/BoardHelpers.java $AI_PATH/helper/Utility.java $AI_PATH/dataStructs/StateNode.java #$TESTER_PATH ArtificialTester.java $TESTER_PATH/RandomTester.java
 
 #javac -d ./bin -sourcepath ./src ./ConnectGame/src/gameElements/Game.java
+
+javadoc -sourcepath ./src -classpath ./src $GAME_ELEMS_PATH/Game.java $GAME_ELEMS_PATH/Player.java $GAME_ELEMS_PATH/Board.java $GAME_ELEMS_PATH/QuadraticBoard.java $GAME_ELEMS_PATH/InputPlayer.java $GAME_ELEMS_PATH/Piece.java $GAME_ELEMS_PATH/StandardBoard.java $AI_PATH/ArtificialPlayer.java $AI_PATH/helper/BoardHelpers.java $AI_PATH/helper/Utility.java $AI_PATH/dataStructs/StateNode.java -d ./docs
 
